288979f29e3d446a48d0ad3a0fcb66450b56c03a
Vani Bhatt <94750366+vani4vani@users.noreply.github.com>
2022-06-05 20:27:06 +0530

    Update views.py

--- purchage_list/p_records_app/views.py
+++ purchage_list/p_records_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import *
 from .serializers import ProductItemSerializer
 from .models import ProductItem
 
